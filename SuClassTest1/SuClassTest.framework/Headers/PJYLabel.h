f793733861100f23913935d247580e5382a8efbf
家远 <pjy150shanhuhai@163.com>
2020-02-24 17:49:31 +0800

    888

---
+++ SuClassTest1/SuClassTest.framework/Headers/PJYLabel.h
@@ -0,0 +1,15 @@
+//
+//  PJYLabel.h
+//  Label_html
+//
+//  Created by yiyou on 17/9/12.
+//  Copyright © 2017年 yiyous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJYLabel : UILabel
+
++(void)shouTest;
+
+@end
