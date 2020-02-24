245f3edf69b27eecb5e3bdc223949c2b8186e1c9
家远 <pjy150shanhuhai@163.com>
2020-02-24 14:44:21 +0800

    666

---
+++ SuClassTest.framework/Headers/SuClassTest.h
@@ -0,0 +1,19 @@
+//
+//  SuClassTest.h
+//  SuClassTest
+//
+//  Created by 庞远 on 2020/2/24.
+//  Copyright © 2020年 pjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuClassTest.
+FOUNDATION_EXPORT double SuClassTestVersionNumber;
+
+//! Project version string for SuClassTest.
+FOUNDATION_EXPORT const unsigned char SuClassTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuClassTest/PublicHeader.h>
+
+
